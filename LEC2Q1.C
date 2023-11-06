923c167b84a344b9355dd4016dd5f699bae6510b
khushi-khunt <khuntkhushi506@gmail.com>
2023-11-06 18:29:27 +0530

    assignment 2.1

---
+++ LEC2Q1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+   int a;
+   int b;
+   int c;
+    clrscr();
+    a=12;
+    b=6;
+    c=a+b;
+    printf("sum of %d and %d is % d",a,b,c);
+    getch();
+    }
+
---
+++ LEC2Q2.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+   int a,b;
+   clrscr();
+       a=12;
+       b=6;
+       printf("the sbstraction of %d and %d is : %d",a,b,a-b);
+   getch();
+   }
